163a4b7ab8e398c4e011714a30c6db7736a4af74
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-08-29 00:01:09 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -1,146 +1,105 @@
 package taller3.televisores;
 
 public class TV {
-	
-	//Atributos----------------------------------------------------------------
-	
 	private Marca marca;
 	private int canal;
-	private int volumen;
 	private int precio;
-	private boolean estado; //encendido true, apagado false
+	private boolean estado;
+	private int volumen;
 	Control control;
-	
-	private static int numTV; //conteo de televisores creados
-	
-	
-	//constructor--------------------------------------------------------------
-	public TV(Marca marca, boolean estado) {
-		
-		this.marca = marca;
-		this.estado = estado;
-		numTV++; //conteo de televisores cada que creo un objeto 
+	static int numTV;
+
+	public TV(Marca marca,boolean estado) {
+		this.marca=marca;
+		this.estado=estado;
+		TV.numTV++;
 		this.canal=1;
 		this.volumen=1;
 		this.precio=500;
-		
-		}
-	
-	//mtodos------------------------------------------------------------------
-	
-	//encendido
-	public void turnOn() {
-		estado = true;
 	}
-	
-	
-	public void turnOff() {
-		estado = false;
-	}
-	
-	//-------------------------------------------------------------------
-	//cambiar canal
-	public void canalUp() {
-		if (estado = true && canal>=1 && canal < 120) {
-			canal++;
-		}
-		else {
-			return;
-		}
-		
-	}
-	
-	public void canalDown() {
-		if (estado = true && canal>1 && canal <= 120) {
-			canal--;
-		}
-		else {
-			return;
-		}
-	}
-	
-	//----------------------------------------------------------------
-	//cambiar volumen
-	
-	public void volumenUp() {
-		if (estado=true && volumen>=0 && volumen < 7) {
-			canal++;
-		}
-		else {
-			return;
-		}
-		
-	}
-	
-	public void volumenDown() {
-		if (estado=true && volumen>0 && volumen <= 7) {
-			canal--;
-		}
-		else {
-			return;
-		}
-	}
-	
-	
-	
-	
-	//getters and setters------------------------------------------------------
 
 	public Marca getMarca() {
 		return marca;
 	}
-
-	public void setMarca(Marca marca) {
-		this.marca = marca;
-	}
-
-	public int getCanal() {
-		return canal;
-	}
-
-	public void setCanal(int canal) {
-		if (estado = true && canal>=1 && canal <= 120) {
-			this.canal=canal;
-		}
-		else {
-			return;
-		}
-	}
-
-	public int getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(int precio) {
-		this.precio = precio;
-	}
-
-	public int getVolumen() {
-		return volumen;
-	}
-
-	public void setVolumen(int volumen) {
-		if (estado=true && volumen>=0 && volumen <= 7) {
-			this.volumen=volumen;
-		}
-		else {
-			return;
-		}
-	}
 	
+	public void setMarca(Marca marca) {
+		this.marca=marca;
+	}
 
 	public Control getControl() {
 		return control;
 	}
-
+	
 	public void setControl(Control control) {
-		this.control = control;
+		this.control=control;
 	}
 	
-	public boolean isEstado() {
+	public int getPrecio() {
+		return precio;
+	}
+	
+	public void setPrecio(int precio) {
+		this.precio=precio;
+	}
+	public int getVolumen() {
+		return volumen;
+	}
+	
+	public void setVolumen(int volumen) {
+		this.volumen=volumen;
+	}
+	public int getCanal() {
+		return canal;
+	}
+	
+	public void setCanal(int canal) {
+		if (estado!=true || canal>120 || canal<1) {
+			return;
+		}
+		this.canal=canal;
+	}
+	
+	
+	public void turnOn() {
+		estado=true;
+	}
+	
+	public void turnOff() {
+		estado=false;
+	}
+	
+	public boolean getEstado() {
 		return estado;
 	}
-
+	
+	public void canalUp() {
+		if (estado!=true || canal==120) {
+			return;
+		}
+		canal++;
+	}
+	
+	public void canalDown() {
+		if (estado!=true || canal==0) {
+			return;
+		}
+		canal--;
+	}
+	
+	public void volumenUp() {
+		if (estado!=true || volumen==7) {
+			return;
+		}
+		volumen++;
+	}
+	
+	public void volumenDown() {
+		if (estado!=true || volumen==0) {
+			return;
+		}
+		volumen--;
+	}
+	
 	public static int getNumTV() {
 		return numTV;
 	}
@@ -149,22 +108,4 @@ public class TV {
 		TV.numTV = numTV;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-		
-
 }
-	
-	
-
-
